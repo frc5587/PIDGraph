ecd23b9d729d0d0932f8d87039fefa9cc2b44929
Drew Fisher <drewsapple111@gmail.com>
2017-03-22 17:12:28 -0400

    Added team 27's code

---
+++ ShooterTuning.js
@@ -0,0 +1,153 @@
+"use strict";
+
+//var dataActual=[];
+var data=[];
+
+var actualVal;
+var requestedVal;
+
+var lineChart;
+var labels = [];
+var i = 0;
+var recording = false;
+var count1 = 1;
+var count2 = 1;
+
+var chartLabels = ['Time', 'Requested Val', 'Actual Val', 'P', 'I', 'D'];
+
+$(document).ready(function(){
+	// sets a function that will be called when any NetworkTables key/value changes
+	NetworkTables.addGlobalListener(onValueChanged, true);
+
+	initFromLocalStorage();
+
+	$('#btnSendParms').click(function(){
+		sendParms()
+	});
+
+	$('#toggleRecording').change(function() {
+		count1+=1;
+		if(count1 % 2 === 0){
+			recording = true;
+		}
+		else{
+			recording = false;
+		}
+	});
+	$('#toggleStreaming').change(function() {
+
+	   if ($(this).prop('checked')) {
+			i = 0;
+			data = [];
+			window.IntervalId = setInterval(function() {
+
+				i = i + 1;
+
+				var y1 = parseInt($("#val").val());
+				var y2 = actualVal;
+				var p = parseInt($('#pGain').val());
+				var i = parseInt($('#iGain').val());
+				var d = parseInt($('#dGain').val());
+
+				//if (i < 5) { alert(y1 + '   ' + y2 + (i/10.0));}
+				data.push([i/10.0, y1, y2, p, i, d]);
+
+				if (i > 120) {
+					data.shift();
+				}
+				if(recording && i % 120 === 0 && i > 0){
+	  			saveData();
+	  		}
+				//file can point to an actual system file or an array
+				lineChart.updateOptions( { 'file': data } );
+				}, 100);
+	   }
+	   else {
+			clearInterval(window.IntervalId);
+	   }
+	});
+
+	createchart();
+	//NetworkTables.addKeyListener('velocity', onVelocityChanged, true);
+	});
+	function saveData(){
+		var now = new Date();
+		var year = now.getFullYear();
+		var month = now.getMonth()+1;
+		var day = now.getDate();
+		var hour = now.getHours();
+		var minute = now.getMinutes();
+		var second = now.getSeconds();
+		var csv = Papa.unparse({
+			fields: chartLabels,
+			data: data
+		});
+		var fileName = (year + "-" + month + "-" + day + "_" + hour + "-" + minute + "-" + second + ".csv");
+		var prefix = 'data:text/csv;charset=utf-8,';
+		var fileData = encodeURI(prefix + csv);
+		var link = document.createElement('a');
+
+		link.setAttribute('href', fileData);
+		link.setAttribute('download', fileName);
+		link.click();
+	}
+
+function sendParms() {
+	NetworkTables.putValue('/arm/setPoint', $('#val').val());
+	NetworkTables.putValue('/arm/pGain', $('#pGain').val());
+	NetworkTables.putValue('/arm/iGain', $('#iGain').val());
+	NetworkTables.putValue('/arm/dGain', $('#dGain').val());
+
+	Lockr.set('setPoint', $('#val').val());
+	Lockr.set('pGain', $('#pGain').val());
+	Lockr.set('iGain', $('#iGain').val());
+	Lockr.set('dGain', $('#dGain').val());
+}
+
+function initFromLocalStorage() {
+	$('#val').val(Lockr.get('setPoint'));
+	$('#pGain').val(Lockr.get('pGain'));
+	$('#iGain').val(Lockr.get('iGain'));
+	$('#dGain').val(Lockr.get('dGain'));
+}
+
+function onValueChanged(key, value, isNew) {
+
+	// key thing here: we're using the various NetworkTable keys as
+	// the id of the elements that we're appending, for simplicity. However,
+	// the key names aren't always valid HTML identifiers, so we use
+	// the NetworkTables.keyToId() function to convert them appropriately
+
+	if (isNew) {
+		var tr = $('<tr/>').appendTo($('#nt > tbody:last'));
+		$('<td/>').text(key).appendTo(tr);
+		$('<td></td>').attr('id', NetworkTables.keyToId(key))
+					   .text(value)
+					   .appendTo(tr);
+	} else {
+
+		// similarly, use keySelector to convert the key to a valid jQuery
+		// selector. This should work for class names also, not just for ids
+		$('#' + NetworkTables.keySelector(key)).text(value);
+	}
+	if (key == '/arm/val') {
+		shooter_left_actualRPM = Math.abs(parseInt(value));
+	}
+
+}
+
+
+function createchart() {
+	lineChart = new Dygraph(document.getElementById("chart"),
+							data,
+							{
+							height: 400,
+							width: 950,
+							colors: ["rgb(40,220,40)", "rgb(0, 62, 126)", "rgb(40,220,40)", "rgb(254,205,10)"],
+							drawPoints: true,
+							showRoller: false,
+							valueRange: [-10, 100],
+							labels: chartLabels
+							});
+
+}
